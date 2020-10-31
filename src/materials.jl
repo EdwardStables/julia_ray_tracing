616cd7ce82f2f1900baafb1fb9dd2dc9bf0c2cdd
Edward Stables <edward.stables1198@gmail.com>
2020-10-31 00:49:12 +0000

    added patterns

--- src/materials.jl
+++ src/materials.jl
@@ -14,6 +14,8 @@ end
 get_color(m::material, p::point, obj::primitive) = get_color(m.color, p, obj)
 get_color(c::color, ::point, ::primitive) = c
 get_color(pat::pattern, p::point, obj::primitive) = pattern_at_object(pat, obj, p)
+set_color(m::material, c::color) = setfield!(m, :color, c)
+set_color(m::material, p::pattern) = setfield!(m, :color, p)
 
 function ==(m1::material, m2::material)
     return m1.color == m2.color &&
@@ -22,3 +24,5 @@ function ==(m1::material, m2::material)
            m1.specular == m2.specular &&
            m1.shininess == m2.shininess
 end
+
+set_transform(m::material, t::Matrix) = set_transform(m.color, t)
